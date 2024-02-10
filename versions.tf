2da2826a0463758620d93816f69c1981096c4009
Andre Silva <andreswebs@pm.me>
2024-02-10 17:29:21 -0300

    upgrade versions

--- versions.tf
+++ versions.tf
@@ -1,11 +1,11 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = "~> 1.5"
 
   required_providers {
 
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.46.0"
+      version = "~> 5.0"
     }
 
   }
